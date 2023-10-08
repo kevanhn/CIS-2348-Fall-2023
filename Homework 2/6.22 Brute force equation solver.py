707e3946ee8a306a9693d6b1050a4f589fbae80b
Kevin Nguyen <112019709+kevanhn@users.noreply.github.com>
2023-10-07 22:28:37 -0500

    Add files via upload

---
+++ Homework 2/6.22 Brute force equation solver.py
@@ -0,0 +1,20 @@
+''' Read in first equation, ax + by = c '''
+a = int(input())
+b = int(input())
+c = int(input())
+
+''' Read in second equation, dx + ey = f '''
+d = int(input())
+e = int(input())
+f = int(input())
+
+solution = False
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        if a * x + b * y == c and d * x + e * y == f:
+            print(x,y)
+            solution = True
+            break
+
+if not solution:
+    print('No solution')
