066a738845ab348b94f0be99b2854347f0559878
Kevin Nguyen <112019709+kevanhn@users.noreply.github.com>
2023-10-08 13:51:15 -0500

    Add files via upload

---
+++ Homework 2/9.10 Word frequencies.py
@@ -0,0 +1,26 @@
+import csv
+
+def main():
+    input_filename = input()
+
+    file = open(input_filename)
+
+    word_counts = csv.reader(file, delimiter=',')
+
+    words = []
+
+    for row in word_counts:
+        for word in row:
+            words.append(word.strip())
+
+    for i in range(len(words)):
+        if words[i] not in words[:i]:
+            count = 0
+            for w in words:
+                if words[i] == w:
+                    count += 1
+            print(words[i], count)
+    file.close()
+
+if __name__ == "__main__":
+    main()
